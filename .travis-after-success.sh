13916c4afcec979875763c42888874eefb4d7228
Scott Wong <wyuleo@gmail.com>
2020-04-28 16:31:30 +0800

    Update .travis-after-success.sh

--- .travis-after-success.sh
+++ .travis-after-success.sh
@@ -19,5 +19,5 @@ cd ${GCR_REPO}
 if [ -n "$(git status --porcelain)" ]; then
   git add .
   git commit -m "Travis CI Auto Update(`date +'%Y-%m-%d %H:%M:%S'`)"
-  git push https://mritd:${GITHUB_TOKEN}@github.com/mritd/gcr.git
+  git push https://scott-wong:${GITHUB_TOKEN}@github.com/scott-wong/gcr.git
 fi
